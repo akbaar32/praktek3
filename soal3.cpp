6b7de3c5671759270d395d84aeb86ef59abed79d
Muhammad Akbar  Prasetya <>
2017-11-08 01:27:24 +0700

    proses perhitungan

--- soal3.cpp
+++ soal3.cpp
@@ -12,5 +12,10 @@ int main(){
 	int gaber;
 	cout<<"masukan jam kerja anda:"; cin >> jamker;
 	cout<<"masukan gajih anda perjam:"; cin >> gajihph;
+	lembur=jamker-40;
+	jamlembur=lembur*1.5;
+	jamkertotal=40+jamlembur;
+	gatot=jamkertotal*gajihph;
+	gaber=gatot-(0.15*gatot);
 }
 
